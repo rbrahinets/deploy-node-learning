3467e564589f51aa9ef1913269b7e29e79ef1751
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-13 13:09:14 +0300

    Update server

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.get('/users', async (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: 'Rostyslav',
+            age: '21',
+        },
+        {
+            id: 2,
+            name: 'John',
+            age: '18',
+        },
+        {
+            id: 3,
+            name: 'Tom',
+            age: '26',
+        },
+    ]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
